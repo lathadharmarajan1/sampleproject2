e7f52099ee3621fc2232bb26638735757c4dcf22
lathadharmarajan1 <latha.dharmarajan1@gmail.com>
2023-03-02 18:19:49 +0530

    new program

---
+++ src/test/java/org/sampleframework/LogicalProgram.java
@@ -0,0 +1,13 @@
+package org.sampleframework;
+
+public class LogicalProgram {
+//string methods
+	public static void main(String[] args) {
+		String s= "kanushika";
+		int length = s.length();
+		System.out.println("length of given string is " +length);
+		
+	}
+	
+	
+	}
